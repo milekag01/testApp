1b65dbff2442f515a8dcba40bba801c5145ab7a4
milekag01 <milekag01@gmail.com>
2019-06-28 02:37:18 +0530

    drawer navigator

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -4,7 +4,13 @@ import {View, Text} from 'react-native';
 class Home extends React.Component {
     static navigationOptions = {
         title: 'Home',
-        
+        headerStyle: {
+            backgroundColor: '#ea1744'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
     };
 
     render() {
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {View, Platform} from 'react-native';
-import {DISHES} from '../shared/dishes';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
-import {createStackNavigator, createAppContainer} from 'react-navigation';
+import {createStackNavigator, createAppContainer, createDrawerNavigator} from 'react-navigation';
 
 
 
@@ -15,8 +15,33 @@ const MenuNavigator = createStackNavigator({
     initialRouteName: 'Menu',
 });
 
+//home navigator(stack wala to get status bar)
+const HomeNavigator = createStackNavigator({
+    Home: {screen: Home}
+});
+
+//creating a drawer navigator
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        navigationOptions: {
+            title: 'Home',
+            drawerLabel: 'Home'
+        }
+    },
+    Menu: {
+        screen: MenuNavigator,
+        navigationOptions: {
+            title: 'Menu',
+            drawerLabel: 'Menu'
+        }
+    }
+},{
+    drawerBackgroundColor: '#f7f7f7'
+});
+
 //react navigation v3
-const Container = createAppContainer(MenuNavigator);
+const Container = createAppContainer(MainNavigator);
 
 class Main extends React.Component {
     
